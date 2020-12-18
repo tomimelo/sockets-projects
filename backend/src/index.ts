7648cc242e80000ef8a8cd2b67deaf1b3d876a5f
Tomas Melone <tomasmelone@hotmail.com>
2020-12-18 23:11:45 +0100

    Added static files support

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,16 +1,22 @@
 import Server from "./classes/server";
 import { SERVER_PORT } from "./global/environment";
 import router from "./routes/router";
-import { json } from "express";
+import express from "express";
 import cors from "cors";
+import path from "path";
 
 const server = Server.instance;
 
-server.app.use(json());
+server.app.use(express.json());
+server.app.use(express.static("public"));
 
 server.app.use( (req, res, next) => { next(); }, cors({origin: true, credentials: true}) );
 
 server.app.use('/api', router);
+
+server.app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'public/index.html'));
+});
 
 server.start( () => {
     console.log(`Server running on port ${SERVER_PORT}`);
